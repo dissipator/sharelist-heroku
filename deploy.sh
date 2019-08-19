4b142e1c5bcdbd66aa6ae04565d3c4d2ef7248ac
dissipator <dissipator@users.noreply.github.com>
2019-08-19 18:43:11 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,17 @@ apk add --update openssl
 
 cd /app
 
-git clone https://github.com/dissipator/sharelist.git
+wget https://github.com/reruin/sharelist/archive/master.zip -O sharelist.zip
+unzip sharelist.zip
 
-cd /app/sharelist
+cd /app/sharelist-master
+
 npm install
 
 if echo "$CONFIG" | grep -q -i "^http"; then
-  wget --no-check-certificate $CONFIG -O /app/sharelist/cache/config.json
+  wget --no-check-certificate $CONFIG -O /app/sharelist-master/cache/config.json
 else
-  echo -e "$CONFIG" > /app/sharelist/cache/config.json
+  echo -e "$CONFIG" > /app/sharelist-master/cache/config.json
 fi
 
 npm start
